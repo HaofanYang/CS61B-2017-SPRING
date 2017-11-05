313aa630d134f40f15035d342cfad35a215528a6
HaofanYang <haofan.yang@icloud.com>
2017-11-05 19:23:25 +0000

    demo ArrayDeque

--- project01/ArrayDeque.java
+++ project01/ArrayDeque.java
@@ -1,35 +1,83 @@
-Public class ArrayDeque<datatype>{
+public class ArrayDeque<datatype>{
 
-    private datatype[3] sentinel;
+    /* 
+    Circular structure, namely we start adding items from the middle of an array
+
+    e.g.
+        nextFirst
+           | nextLast
+           | |
+     0 1 2 3 4 5 6
+    [0,0,0,0,0,0,0]  (this is a empty list)
+    size = 0
+    nextFirst = 3 (this is the index of the array where you add an item by using 'addFirst')
+    nextLast = 4
+
+    if we implement addFirst(5), we will get：
+      nextFist
+         |  nextLast
+         |   |
+     0 1 2 3 4 5 6
+    [0,0,0,5,0,0,0]  (this is a empty list)
+    size = 1
+    nextFirst = 2
+    nextLast = 4          
+    */
+
+
+    private datatype[] items;
     private int size;
+    public int nextLast;
+    public int nextFirst;
 
     /** construct an empty list*/
-    public void ArrayDeque(){
-        sentinel = new ;
-        sentinel.next = sentinel;
-        sentinel.previous = sentinel.next;
+    public ArrayDeque(){
+        items = (datatype[]) new Object[7];
         size = 0;
+        nextFirst = 3;
+        nextLast = 4;
     }
 
-    /** a precious of sentinel is itself and so does next*/
-    public void ArrayDeque(datatype i){
-        sentinel = new StuffNode(null, null, null);
-        sentinel.next = new StuffNode(sentinel, i, sentinel.next);
-        sentinel.previous = sentinel.next;
-        size = 1;
+    /*
+    private void resizing(){
+        datatype[] a = (datatype[]) new Object[size * 2];
+        System.arraycopy(items, 0, a, nextFirst - 1, size);
+        items = a;
+    }
+    */
+
+    /* increase a postion by 1, if the pointer is the end of the array, retrun to 0 */
+    private int positionPlus(int p){
+        if (p >= items.length - 1){
+            p = 0;
+        }else{
+            p += 1;
+        }
+        return p;
     }
 
+    /* decrease a postion by 1, if the pointer is 0, retrun to the end of the array */
+    private int positionMinus(int p){
+        if (p == 0){
+            p = items.length - 1;
+        }else{
+            p -= 1;
+        }
+        return p;
+    }
 
-    /** add i at the front of the list */
+    /** TODO consider resizing and circular*/
     public void addFirst(datatype i){
-        sentinel.next = new StuffNode(sentinel, i, sentinel.next);
-        size = size + 1;
+        items[nextFirst] = i;
+        size += 1;
+        nextFirst = positionMinus(nextFirst);
     }
 
-    /** add i at the end of the list */
+    /** TODO consider circular data structure*/
     public void addLast(datatype l){
-        sentinel.previous = new StuffNode(sentinel.previous, l, sentinel);
-        size = size + 1;
+        items[nextLast] = l;
+        size += 1;
+        nextLast = positionPlus(nextLast);
     }
 
     /** tell if a list is empty */
@@ -42,36 +90,40 @@ Public class ArrayDeque<datatype>{
         return size;
     }
 
-    /** print out the list */
+    /** Print out the Deque */
+    /** TODO boundary case!!! when the array is full */
     public void printDeque(){
-        if (this.isEmpty()){
-            System.out.println("the queue is empty");
-            return;
-        }
-        ArrayDeque p = sentinel;
-        p.sentinel = p.sentinel.next;
-        while (p.next != sentinel){
-            System.out.print(p.sentinel.item);
+        int start = positionPlus(nextFirst);
+        int end = positionMinus(nextLast);
+        int count = start;
+        while (count != end){
+            System.out.print(items[count]);
             System.out.print(" ");
-            p.sentinel = p.sentinel.next;
+            count = positionPlus(count);
         }
     }
 
-    /** Remove the first item of the queue */
+    /** remove the first item */
     public datatype removeFirst(){
-        datatype result = sentinel.next.item;
-        sentinel.next = new StuffNode(sentinel)
+        nextFirst = positionMinus(nextFirst);
+        datatype result = items[nextFirst];
+        items[nextFirst] = null;
+        size -= 1;
+        return result;
+    }
+
+    /** remove the last item */
+    public datatype removeLast(){
+        nextLast = positionMinus(nextLast);
+        datatype result = items[nextLast];
+        items[nextLast] = null;
         return result;
     }
 
     /** TODO */
-    public Item removeLast(){
+    public datatype get(int index){
         return null;
     }
 
-    /** TODO */
-    public Item get(int index){
-        return null;
-    }
 
 }
--- project01/Test.java
+++ project01/Test.java
@@ -1,12 +1,33 @@
-import java.util.*;
-public class Test{
-    public static void main(String[] args){
-        LinkedListDeque<Integer> l = new LinkedListDeque<Integer>(3);
 
-        l.addFirst(5);
-        l.addLast(6);
-        l.printDeque();
-        //l.removeFirst()
-        //System.out.println(l.get(1));
-    }
+public class Test{
+	public static void main(String[] args){
+		ArrayDeque<String> a = new ArrayDeque<>();
+		/*
+		System.out.println(a.size());
+		System.out.println(a.nextFirst);
+		System.out.println(a.nextLast);
+		a.addFirst("a");
+		System.out.println(a.nextFirst);
+		System.out.println(a.nextLast);
+		System.out.println(a.size());
+		*/
+	
+		a.addFirst("c");
+		a.addLast("b");
+		a.addLast("a");
+		a.addLast("a");
+		a.addLast("a");
+		a.addFirst("a");
+		a.addLast("fuck you");
+
+		a.printDeque();
+		System.out.println(" ");
+		System.out.print("nextFirst = ");
+		System.out.println(a.nextFirst);
+		System.out.print("nextLast = ");
+		System.out.println(a.nextLast);
+		System.out.print("size = ");
+		System.out.println(a.size());
+
+	}
 }
